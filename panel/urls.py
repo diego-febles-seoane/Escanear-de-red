f4a93ff21398c041b689716653df71f6f3726a31
Alejandro Donate García <AlexDoGa.work@gmail.com>
2026-06-03 13:50:03 +0100

    Se añadio una aleta de tiburon a la ola de carga en topologia y se cambio la pantalla principal para que sea topologia

--- panel/urls.py
+++ panel/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.index, name='index'),
+    path('', views.topologia_page, name='index'),
     path('inventario/', views.inventario_page, name='inventario'),
     path('scan/', views.scan, name='scan'),
     path('get-devices/', views.get_devices, name='get_devices'),
